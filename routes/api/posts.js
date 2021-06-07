aca8e7be3eb85f700f11d635a0697963b20fddd7
iqbalfaisal <7faisaliqbal@gmail.com>
2021-06-07 10:59:06 +0500

    get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -21,8 +21,6 @@ router.post(
     try {
       const user = await User.findById(req.user.id).select("-password");
 
-      console.log(user);
-
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
@@ -39,5 +37,19 @@ router.post(
     }
   }
 );
+
+//@route GET api/posts
+//@desc Get all post
+//@access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
